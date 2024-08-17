13503daf2bc9c9fe6f60f9e9b1026391e5db95d1
eduardohen1 <eduardohmferreira@gmail.com>
2024-08-17 10:01:46 -0300

    Iniciando aplicação com Spring AI

---
+++ MovieSugestion/src/main/java/es/minsait/MovieSugestion/MovieSugestionApplication.java
@@ -0,0 +1,13 @@
+package es.minsait.MovieSugestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSugestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSugestionApplication.class, args);
+	}
+
+}
