29c927df1f01ef1750a839d2ce911ae45f4c3821
eduardohen1 <eduardohmferreira@gmail.com>
2024-08-24 10:50:00 -0300

    Iniciando com Langchain4j 04

--- LangchainDemo/src/main/java/es/com/minsait/LangchainDemo/controller/OpenAIController.java
+++ LangchainDemo/src/main/java/es/com/minsait/LangchainDemo/controller/OpenAIController.java
@@ -5,6 +5,8 @@ import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
 import es.com.minsait.LangchainDemo.dto.MyQuestion;
 import es.com.minsait.LangchainDemo.dto.StructuredTemplate;
+import es.com.minsait.LangchainDemo.rag.Assistant;
+import es.com.minsait.LangchainDemo.rag.RAGConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -13,9 +15,13 @@ import java.util.Arrays;
 public class OpenAIController {
 
     private ChatLanguageModel chatLanguageModel;
+    private RAGConfiguration ragConfiguration;
+    private Assistant assistant;
 
-    public OpenAIController(ChatLanguageModel chatLanguageModel) {
+    public OpenAIController(ChatLanguageModel chatLanguageModel,
+                            RAGConfiguration ragConfiguration) {
         this.chatLanguageModel = chatLanguageModel;
+        this.ragConfiguration = ragConfiguration;
     }
 
     @PostMapping("/chat")
@@ -38,5 +44,17 @@ public class OpenAIController {
         return chatLanguageModel.generate(prompt.text());
 
     }
+
+    @PostMapping("/chatrag")
+    public String chatComMemoriaRAG(@RequestBody MyQuestion question){
+        try {
+            if(assistant == null)
+                assistant = ragConfiguration.configure();
+            return assistant.answer(question.question());
+        }catch (Exception ex){
+            return "Erro: " + ex.getMessage();
+        }
+    }
+
 
 }
